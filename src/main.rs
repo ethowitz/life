ed6ce81d54be37f00fcdb639475c3cdcd1c1c598
Ethan Donowitz <ethan.donowitz@posteo.org>
2017-03-28 21:57:46 -0400

    made one of the functions SO much better

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{env, thread, time};
 extern crate rand;
 
+const ALIVE: char = 'x';
+
 fn main() {
     let argv: Vec<String> = env::args().collect();
     let rows = argv[1].parse::<usize>().unwrap();
@@ -17,38 +19,18 @@ fn main() {
     }
 }
 
+// i resent this function
 fn get_live_neighbors(board: &Vec<Vec<char>>, row: usize, col: usize) -> u8 {
     let mut count: u8 = 0;
-    for i in 0..2 {
-        for j in 0..2 {
-            if (i != 0 || j != 0) && row + i < board.len() && col + j < board[0].len() &&
-               board[row + i][col + j] == 'X' {
+    for i in 0..3 {
+        for j in 0..3 {
+            if (i != 1 || j != 1) && (row + i) as i32 - 1 >= 0 && (col + j) as i32 - 1 >= 0 &&
+               row + i - 1 < board.len() && col + j - 1 < board[0].len() &&
+               board[row + i - 1][col + j - 1] == ALIVE {
                 count += 1;
             }
         }
     }
-
-    if row > 0 {
-        for j in 0..2 {
-            if row - 1 < board.len() && col + j < board[0].len() &&
-               board[row - 1][col + j] == 'X' {
-                count += 1;
-            }
-        }
-    }
-
-    if col > 0 {
-        for i in 0..2 {
-            if row + i < board.len() && col - 1 < board[0].len() && board[row + i][col - 1] == 'X' {
-                count += 1;
-            }
-        }
-    }
-
-    if col > 0 && row > 0 && board[row - 1][col - 1] == 'X' {
-        count += 1;
-    }
-
     count
 }
 
@@ -57,10 +39,10 @@ fn apply_rules(board: &Vec<Vec<char>>, rows: usize, cols: usize) -> Vec<Vec<char
     for i in 0..board.len() {
         for j in 0..board[0].len() {
             let count = get_live_neighbors(board, i, j);
-            if board[i][j] == 'X' && (count < 2 || count > 3) {
+            if board[i][j] == ALIVE && (count < 2 || count > 3) {
                 new_board[i][j] = ' ';
             } else if board[i][j] == ' ' && count == 3 {
-                new_board[i][j] = 'X';
+                new_board[i][j] = ALIVE;
             } else {
                 new_board[i][j] = board[i][j];
             }
@@ -70,7 +52,7 @@ fn apply_rules(board: &Vec<Vec<char>>, rows: usize, cols: usize) -> Vec<Vec<char
 }
 
 fn generate_seed(board: &mut Vec<Vec<char>>) {
-    let gen_char = |x| if rand::random::<i64>() % 4 == 1 {'X'} else {' '};
+    let gen_char = |x| if rand::random::<i64>() % 4 == 1 {ALIVE} else {' '};
     for row in board {
         *row = row.into_iter().map(&gen_char).collect();
     }
